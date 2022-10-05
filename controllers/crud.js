9e847c2bd39e97e2f99a55ea1325b0584c853727
JHE4N <93744970+JHE4N@users.noreply.github.com>
2022-10-05 16:49:41 -0300

    commit inicial

---
+++ controllers/crud.js
@@ -0,0 +1,41 @@
+
+const connection = require('../database/db')
+
+exports.save = (req, res) => {
+    const client = req.body.user
+    const property = req.body.property
+    const value = req.body.value
+    console.log(client+' '+property+' '+value);
+    connection.query(
+        'INSERT INTO crud(id, client, property, value) VALUES($1, $2, $3, $4)',
+        [createHash(client, property, value), client, property, value],
+        (error, results) => {
+            if(error) {
+                console.log(error);
+            } else {
+                res.redirect('/');         
+            }
+    })
+}
+
+exports.update = (req, res) => {
+    const id = req.body.id
+    const client = req.body.user
+    const property = req.body.property
+    const value = req.body.value
+    console.log(client+' '+property+' '+value);
+    connection.query(
+        'UPDATE crud SET client=$1, property=$2, value=$3, id=$4 WHERE id=$4',
+        [client, property, value, id],
+        (error, results) => {
+            if(error) {
+                console.log(error);
+            } else {
+                res.redirect('/');         
+            }
+    })
+}
+
+function createHash(client, property, value) {
+    return Number(client.toString().length) + Number(property.toString().length) + Number(value)
+}
